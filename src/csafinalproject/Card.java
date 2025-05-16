d25c63460564d144f053df435dceca920feec3dc
Shane <shanemacnicoll@gmail.com>
2025-05-16 08:21:34 -0400

    card class

--- src/csafinalproject/Card.java
+++ src/csafinalproject/Card.java
@@ -1,5 +1,25 @@
 package csafinalproject;
 
 public class Card {
+    public enum Suit { BLUE, GREEN, PURPLE, YELLOW };
     
+    private int number;
+    private Suit suit;
+    
+    public Card(int number, Suit suit) {
+        this.number = number;
+        this.suit = suit;
+    }
+    
+    public int getNumber() {
+        return number;
+    }
+    
+    public Suit getSuit() {
+        return suit;
+    }
+    
+    public boolean compare(Card card) {
+        return number == card.getNumber() || suit == card.getSuit();
+    }
 }
