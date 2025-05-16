26352eefbc190b687a1865bb107ec5ad5132347b
Shane <shanemacnicoll@gmail.com>
2025-05-16 08:07:11 -0400

    game class

--- src/csafinalproject/Game.java
+++ src/csafinalproject/Game.java
@@ -1,5 +1,35 @@
 package csafinalproject;
 
-public class Game {
+import javax.swing.JPanel;
+
+public class Game extends JPanel {
+    private Deck drawPile;
+    private Deck discardPile;
     
+    private Player[] players;
+    private int turnID;
+    
+    public Player getNextPlayer() {
+        return players[(turnID + 1) % players.length];
+    }
+    
+    public Player getPreviousPlayer() {
+        return players[(turnID - 1) % players.length];
+    }
+    
+    public Deck getDrawPile() {
+        return drawPile;
+    }
+    
+    public Deck getDiscardPile() {
+        return discardPile;
+    }
+    
+    public Card getCurrentCard() {
+        return null;
+    }
+    
+    private void dealCards() {
+        // Deal 7 cards to each player's deck
+    }
 }
