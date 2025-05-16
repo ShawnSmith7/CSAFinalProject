60139349159baf6735fd75366c39531970e78909
3012535 <3012535@10.50.12.190>
2025-05-16 08:24:43 -0400

    updating player class

--- src/csafinalproject/Player.java
+++ src/csafinalproject/Player.java
@@ -1,5 +1,30 @@
 package csafinalproject;
 
 public class Player {
+    private Deck hand;
     
+    public Player(Deck _hand) {
+        hand = _hand;
+    }
+    
+    public Deck getHand() {
+        return hand;
+    }
+    
+    public void setHand(Deck _hand) {
+        hand = _hand;
+    }
+    
+    public void addCard() {
+        //deck methods dont exist yet
+        //hand.addCard();
+    }
+    
+    public void discard(int index) {
+        //hand.discard(index);
+    }
+    
+    protected void draw(int numberOfCards, Deck deck) {
+        
+    }
 }
