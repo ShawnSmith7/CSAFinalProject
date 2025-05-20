7d7b7cf3bef6cc74675806ace829d9858a36950d
3012770 <3012770@10.50.12.174>
2025-05-20 11:19:07 -0400

    Computer Class

--- src/csafinalproject/Card.java
+++ src/csafinalproject/Card.java
@@ -2,4 +2,65 @@ package csafinalproject;
 
 public class Card {
     
+    public int num;
+    public enum Suit{red, green, blue, yellow, };
+    public enum Action{wild, reverse, skip, draw2, wilddraw4};
+    public Suit currentSuit;
+    public Action currentAction;
+    
+    public Card(int n, Suit s) {
+        
+        num = n;
+        currentSuit = s;
+    }
+    
+    public Card(Action a, Suit s) {
+        
+        currentAction = a;
+        currentSuit = s;
+    }
+    
+    public Card(Action a) {
+        
+        currentAction = a;
+    }
+    
+    public int getNum() {
+        
+        return num;
+    }
+    
+    public Suit getColor() {
+        
+        return currentSuit;
+    }
+    
+    public Action getAction() {
+        
+        return currentAction;
+    }
+    
+    public boolean compare(Card card) {
+        
+        if(card.getColor() == discardPile(0).getSuit()) {
+            
+            return true;
+        }
+        
+        else if(card.getNum() == discardPile(0).getNum()) {
+            
+            return true;
+        }
+        
+        else {
+            
+            return false;
+        }
+    }
+    
+    public String toString() {
+        
+        return getColor() + " " + getNum();
+    }
+    
 }
